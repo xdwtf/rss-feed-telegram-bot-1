8a76e475e4c614e8355d56512b20bcc9281abf8c
Percy <percy@railway.app>
2021-10-18 02:40:50 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# RSS Feed Telegram Bot
+A bot to post messages to Telegram Groups or Channels from rss feed.
+- This bot can also send /mirror commands to your mirror bot using your telegram account.
+bcoz bot can't read another bot's mag. So this bot will use your TG account to interact with mirror bot.
+Fill `STR_SESSION` and `MIRROR_CHAT_ID` vars to enable it.
+
+## Configuration
+- Edit the [rss.py](./rss.py) as your needs.
+- Edit values in [config.env](./config.env.template) or set it in Environment Variables. There is an template for `config.env` already exists just edit it and rename the file.
+
+### Configuration Values
+- `APP_ID` - Get it from [my.telegram.org](https://my.telegram.org/apps)
+- `API_HASH` - Get it from [my.telegram.org](https://my.telegram.org/apps)
+- `BOT_TOKEN` - Get it by creating a Telegram bot on [BotFather](https://t.me/BotFather)
+- `FEED_URLS` - List of URLs of RSS Feed, sperated by `|` vertical bar.
+- `LOG_CHAT` - ID of the Telegram Chat where messages are to be posted.
+- `DATABASE_URL` - Here is a full [guide](https://github.com/SpEcHiDe/NoPMsBot/wiki/How-to-Install-Database-%3F). For Heroku, just add the `Heroku Postgres` add-on.
+- `INTERVAL` - Checking Interval in seconds. (optional)
+- `MAX_INSTANCES` - Max instances to be used while checking rss feed. (optional)
+### Working as a userbot on your behalf to interact with mirror bot.
+
+These variabls are required only if you want to use your tg account to send /mirror cmd to any mirror bot.
+- `MIRROR_CHAT_ID` - Group/chat_id of mirror chat or mirror bot to send mirror cmd.
+- `MIRROR_CMD` - if you have changed default cmd of mirror bot, replace this.
+- `STR_SESSION` - String session generate using your tg mobile number for sending mirror cmd on your behalf. Generate by running
+```
+python gen_str.py 
+```
+(heroku users run in heroku console)
+
+## Deployment
+
+### Deploying on Railway
+[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template?template=https%3A%2F%2Fgithub.com%2FiamLiquidX%2Frss-feed-telegram-bot&plugins=postgresql&envs=API_ID%2CAPI_HASH%2CFEED_URLS%2CBOT_TOKEN%2CLOG_CHANNEL%2CINTERVAL%2CMAX_INSTANCES%2CSTR_SESSION%2CMIRROR_CHAT_ID%2CMIRROR_CMD&API_IDDesc=Get+it+from+my.telegram.org&API_HASHDesc=Get+it+from+my.telegram.org&FEED_URLSDesc=RSS+Feed+URL+of+the+site.+Split+by++%7C++if+there+are+more+than+one.&BOT_TOKENDesc=Get+it+by+creating+a+bot+on+https%3A%2F%2Ft.me%2Fbotfather&LOG_CHANNELDesc=Create+a+channel+%2C+send+a+message+and+forward+that+message+to+%40username_to_id_bot+%2C+you+will+get+channel+id.&INTERVALDesc=Times+between+checks.&MAX_INSTANCESDesc=2-3+is+more+than+enough.&STR_SESSIONDesc=Fill+this+if+you+wanna+setup+autoleech+or+automirror+system.&MIRROR_CHAT_IDDesc=Only+useful+if+u+filled+string+session+variable.+This+will+send+mirror+commands+on+your+behalf+to+the+mentioned+chat+id.&MIRROR_CMDDesc=Mirror+command+of+your+bot.)
+
+### Deploying on Heroku
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+### VPS or any other server/pc
+
+- Install requirements from [requirements.txt](./requirements.txt)
+```
+pip3 install -r requirements.txt
+```
+- Deploy
+```
+python3 rss.py
+```
+
+## Copyright & License
+- Copyright (©) 2021 by [Adnan Ahmad](https://github.com/viperadnan-git)
+- Licensed under the terms of the [GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007](./LICENSE)
---
+++ rss.py
@@ -0,0 +1,118 @@
+import os
+import sys
+import feedparser
+from sql import db
+from time import sleep
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+try:
+    api_id = int(os.environ["API_ID"])   # Get it from my.telegram.org
+    api_hash = os.environ["API_HASH"]   # Get it from my.telegram.org
+    feed_urls = list(set(i for i in os.environ["FEED_URLS"].split("|")))  # RSS Feed URL of the site.
+    bot_token = os.environ["BOT_TOKEN"]   # Get it by creating a bot on https://t.me/botfather
+    log_channel = int(os.environ["LOG_CHANNEL"])   # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.
+    check_interval = int(os.environ.get("INTERVAL", 10))   # Check Interval in seconds.
+    max_instances = int(os.environ.get("MAX_INSTANCES", 3))   # Max parallel instance to be used.
+    str_session = os.environ.get("STR_SESSION")    #String session generate using your tg mobile number for sending mirror cmd on your behalf. Generate using python gen_str.py
+    mirr_chat = int(os.environ.get("MIRROR_CHAT_ID", "-1"))    #Group/chat_id of mirror chat or mirror bot to send mirror cmd
+    mirr_cmd = os.environ.get("MIRROR_CMD", "/mirror")    #if you have changed default cmd of mirror bot, replace this.
+except Exception as e:
+    print(e)
+    print("One or more variables missing or have error. Exiting !")
+    sys.exit(1)
+
+
+for feed_url in feed_urls:
+    if db.get_link(feed_url) == None:
+        db.update_link(feed_url, "*")
+
+
+app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+app2 = None
+if str_session is not None and str_session != "":
+    app2 = Client(str_session, api_id=api_id, api_hash=api_hash)
+
+
+@app.on_message(filters.command(["up"]))
+async def reply_up_bot(client, msg):
+    if msg.chat.id in [log_channel, mirr_chat]:
+        await msg.reply_text(f"Up Bro")
+
+if app2 is not None:
+    @app2.on_message(filters.command(["up"]))
+    async def reply_up_ub(client, msg):
+        if msg.chat.id in [log_channel, mirr_chat]:
+            await msg.reply_text(f"Up Bro")
+
+def create_feed_checker(feed_url):
+    def check_feed():
+        FEED = feedparser.parse(feed_url)
+
+        if len(FEED.entries) == 0:
+            print(f"RSS Feed at {feed_url} returned no entries")
+            try:
+                app.send_message(log_channel, f"RSS Feed at {feed_url} returned no entries")
+            except FloodWait as e:
+                print(f"FloodWait: {e.x} seconds")
+                sleep(e.x)
+            except Exception as e:
+                print(e)
+
+            return
+
+        first_entry = FEED.entries[0]
+        last_id_from_db = db.get_link(feed_url).link
+
+        if last_id_from_db == "*":
+            message = f"**{first_entry.title}**\n```{first_entry.link}```"
+            try:
+                app.send_message(log_channel, message)
+                if app2 is not None:
+                    mirr_msg = f"{mirr_cmd} {first_entry.link}"
+                    app2.send_message(mirr_chat, mirr_msg)
+            except FloodWait as e:
+                print(f"FloodWait: {e.x} seconds")
+                sleep(e.x)
+            except Exception as e:
+                print(e)
+            db.update_link(feed_url, first_entry.id)
+            return
+
+        for entry_num, entry in enumerate(FEED.entries):
+
+            # Have reached the end of new entries
+            if entry.id == last_id_from_db:
+                # No new entry
+                if entry_num == 0:
+                    print(f"Checked feed for {feed_url}: {entry.id}")
+                break
+
+            # ↓ Edit this message as your needs.
+            message = f"**{entry.title}**\n```{entry.link}```"
+            try:
+                app.send_message(log_channel, message)
+                if app2 is not None:
+                    mirr_msg = f"{mirr_cmd} {entry.link}"
+                    app2.send_message(mirr_chat, mirr_msg)
+            except FloodWait as e:
+                print(f"FloodWait: {e.x} seconds")
+                sleep(e.x)
+            except Exception as e:
+                print(e)
+
+        db.update_link(feed_url, first_entry.id)
+
+    return check_feed
+
+
+scheduler = BackgroundScheduler()
+for feed_url in feed_urls:
+    feed_checker = create_feed_checker(feed_url)
+    scheduler.add_job(feed_checker, "interval", seconds=check_interval, max_instances=max_instances)
+scheduler.start()
+if app2 is not None:
+    app2.start()
+app.run()
