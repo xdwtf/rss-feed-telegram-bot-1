45a60b822d256c293a3239f594ba931295488536
mb807635 <78798667+mb807635@users.noreply.github.com>
2021-10-31 17:24:24 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -62,7 +62,10 @@ def create_feed_checker(feed_url):
         if last_id_from_db == "*":
             message = f"/qbleech ```{first_entry.link}```"
             try:
-                app.send_message(log_channel, message)
+                if "720p.HEVC" in link or "GalaXXXy" in link:
+                    app.send_message(log_channel, message)
+                else:
+                _LOG.info(f"{link}: >>>>>>skip<<<<<<")
                 if app2 is not None:
                     mirr_msg = f"{mirr_cmd} {first_entry.link}"
                     app2.send_message(mirr_chat, mirr_msg)
@@ -85,8 +88,11 @@ def create_feed_checker(feed_url):
 
             # ↓ Edit this message as your needs.
             message = f"**/qbleech ```{entry.link}```"
-            try:
-                app.send_message(log_channel, message)
+            try: 
+                if "hevc" in link or "x265" in link:
+                    app.send_message(log_channel, message)
+                else:
+                _LOG.info(f"{link}: >>>>>>skip<<<<<<")
                 if app2 is not None:
                     mirr_msg = f"{mirr_cmd} {entry.link}"
                     app2.send_message(mirr_chat, mirr_msg)
